35acfc156c44b7808b974841068b712e69080c8a
Chalece Delacoudray <chalecedelacoudray@Mac.attlocal.net>
2026-03-25 14:38:37 -0400

    Configure Vite and GitHub Pages deployment route matching fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './App.css';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/defense-tapboard">
       <GameProvider>
         <Routes>
           <Route element={<Layout />}>
